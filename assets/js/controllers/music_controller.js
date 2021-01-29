02dcc50f045100f987bb8015d81a904050801414
YouST <adek06@163.com>
2021-01-29 13:36:46 +0800

    feat(music): add function for upload music

--- assets/js/controllers/music_controller.js
+++ assets/js/controllers/music_controller.js
@@ -1,11 +1,36 @@
 // music_controller.js
 import { Controller } from "stimulus"
+let OSS = require('ali-oss')
+
+
 
 export default class extends Controller {
-  static targets = [ "name", "output" ]
+    static targets = [ "name", "output", "musicFile" ]
+    test() {
+        console.log(this.musicFileTarget)
+        console.log(musicFile.id)
+        var fileObj = document.getElementById("musicFile").files[0];
+        console.log(fileObj)
+    }
 
-  greet() {
-    this.outputTarget.textContent =
-      `Hello, ${this.nameTarget.value}!`
-  }
+    upload() {
+        let client = new OSS({
+            region: 'oss-cn-beijing',
+            accessKeyId: '',
+            accessKeySecret: '',
+            bucket: 'adek06game'
+        })
+        let file = document.getElementById("musicFile").files[0];
+        client.put('testMusic.mp3', file).then(function (r1) {
+            console.log('success');
+            return client.get('object');
+        }).catch(function (err) {
+            console.error('error: ', err);
+        });
+    }
+
+    greet() {
+        this.outputTarget.textContent =
+            `Hello, ${this.nameTarget.value}!`
+    }
 }
